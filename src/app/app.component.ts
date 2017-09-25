cbb9dac00f08f9de38033872360689b9173a7b7d
Eric Schwartz <eric.schwartz@theironyard.com>
2017-09-25 11:29:52 -0700

    with validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms'
 
 @Component({
@@ -8,6 +8,9 @@ import { NgForm } from '@angular/forms'
 })
 export class AppComponent {
   title = 'app';
+
+  studentForm: NgForm;
+  @ViewChild('studentForm') currentForm: NgForm;
 
 
   model: object = {
@@ -19,4 +22,54 @@ export class AppComponent {
     console.log(data.value)
     console.log(this.model)
   }
+
+  ngAfterViewChecked() {
+    this.formChanged();
+  }
+
+  formChanged() {
+    //if the form didn't change then do nothing
+    if (this.currentForm === this.studentForm) { 
+      return; 
+    }
+    //set the form to the current form for comparison
+    this.studentForm = this.currentForm;
+    //subscribe to form changes and send the changes to the onValueChanged method
+    this.studentForm.valueChanges
+      .subscribe(() => this.onValueChanged());
+  }
+
+  onValueChanged() {
+    let form = this.studentForm.form;
+
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
+  //start out the errors as an empty string
+  formErrors = {
+    'first_name': '',
+    'last_name': ''
+  };
+
+  validationMessages = {
+    'first_name': {
+      'required':      'First name is required.',
+      'minlength':     'Name must be at least 2 characters long.'
+    },
+    'last_name': {
+      'minlength':     'Name must be at least 2 characters long.'
+    }
+  };
+
 }
